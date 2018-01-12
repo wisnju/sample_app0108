d409f663874269295cc147497e4e2bb6a6f78885
wiscas <tzyougnju@foxmail.com>
2018-01-12 16:53:29 +0000

    Use layout file to realize DRY

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,5 +6,30 @@ describe "Static Pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample app')
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Sample_app|Home")
+    end
   end
+  describe "Help page" do
+    it "should have the content 'Help'" do
+    visit '/static_pages/help'
+    expect(page).to have_content('Help')
+    end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Sample_app|Help")
+    end
+  end
+  describe "About page" do
+    it "should have the coontent 'About us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Sample_app|About us")
+    end
+  end
+  
 end
